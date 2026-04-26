890edbb4fa97d316312012abcaac315c9dd0c887
zhangsy <2938637323@qq.com>
2026-04-26 17:27:52 +0800

    优化一下

--- src/components/VirtualJoystick.ts
+++ src/components/VirtualJoystick.ts
@@ -10,6 +10,10 @@ export class VirtualJoystick {
     // On-screen buttons
     private btnLeft!: Phaser.GameObjects.Container;
     private btnRight!: Phaser.GameObjects.Container;
+    private leftBg!: Phaser.GameObjects.Rectangle;
+    private rightBg!: Phaser.GameObjects.Rectangle;
+    private leftArrow!: Phaser.GameObjects.Text;
+    private rightArrow!: Phaser.GameObjects.Text;
     private btnLeftPressed = false;
     private btnRightPressed = false;
 
@@ -18,7 +22,7 @@ export class VirtualJoystick {
 
         // Touch / pointer swipe controls
         this.scene.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-            if (pointer.y < CONSTANTS.SCREEN_HEIGHT - 60) {
+            if (pointer.y < CONSTANTS.SCREEN_HEIGHT - 100) {
                 this.touchActive = true;
                 this.startX = pointer.x;
                 this.currentX = pointer.x;
@@ -43,45 +47,103 @@ export class VirtualJoystick {
         const btnH = 30;
 
         // Left button
-        const leftBg = scene.add.rectangle(0, 0, btnW, btnH, 0xffffff, 0.15)
-            .setStrokeStyle(1, 0xffffff, 0.3);
-        const leftArrow = scene.add.text(0, 0, '<', {
-            fontSize: '16px', color: '#ffffff', fontFamily: 'monospace'
+        this.leftBg = scene.add.rectangle(0, 0, btnW, btnH, 0xffffff, 0.2)
+            .setStrokeStyle(2, 0xffffff, 0.4);
+        this.leftBg.isFilled = true;
+        this.leftBg.isStroked = true;
+        this.leftArrow = scene.add.text(0, 0, '◀', {
+            fontSize: '18px', color: '#ffffff', fontFamily: 'sans-serif'
         }).setOrigin(0.5);
-        this.btnLeft = scene.add.container(35, btnY, [leftBg, leftArrow])
+        this.btnLeft = scene.add.container(35, btnY, [this.leftBg, this.leftArrow])
             .setScrollFactor(0).setDepth(500).setSize(btnW, btnH);
 
         // Right button
-        const rightBg = scene.add.rectangle(0, 0, btnW, btnH, 0xffffff, 0.15)
-            .setStrokeStyle(1, 0xffffff, 0.3);
-        const rightArrow = scene.add.text(0, 0, '>', {
-            fontSize: '16px', color: '#ffffff', fontFamily: 'monospace'
+        this.rightBg = scene.add.rectangle(0, 0, btnW, btnH, 0xffffff, 0.2)
+            .setStrokeStyle(2, 0xffffff, 0.4);
+        this.rightBg.isFilled = true;
+        this.rightBg.isStroked = true;
+        this.rightArrow = scene.add.text(0, 0, '▶', {
+            fontSize: '18px', color: '#ffffff', fontFamily: 'sans-serif'
         }).setOrigin(0.5);
-        this.btnRight = scene.add.container(CONSTANTS.SCREEN_WIDTH - 35, btnY, [rightBg, rightArrow])
+        this.btnRight = scene.add.container(CONSTANTS.SCREEN_WIDTH - 35, btnY, [this.rightBg, this.rightArrow])
             .setScrollFactor(0).setDepth(500).setSize(btnW, btnH);
 
         // Make buttons interactive
         this.btnLeft.setInteractive({ useHandCursor: true });
         this.btnRight.setInteractive({ useHandCursor: true });
 
-        this.btnLeft.on('pointerdown', () => { this.btnLeftPressed = true; });
-        this.btnLeft.on('pointerup', () => { this.btnLeftPressed = false; });
-        this.btnLeft.on('pointerout', () => { this.btnLeftPressed = false; });
+        this.btnLeft.on('pointerdown', () => {
+            this.btnLeftPressed = true;
+            this.updateButtonStyle(this.leftBg, this.leftArrow, true);
+        });
+        this.btnLeft.on('pointerup', () => {
+            this.btnLeftPressed = false;
+            this.updateButtonStyle(this.leftBg, this.leftArrow, false);
+        });
+        this.btnLeft.on('pointerout', () => {
+            this.btnLeftPressed = false;
+            this.updateButtonStyle(this.leftBg, this.leftArrow, false);
+        });
 
-        this.btnRight.on('pointerdown', () => { this.btnRightPressed = true; });
-        this.btnRight.on('pointerup', () => { this.btnRightPressed = false; });
-        this.btnRight.on('pointerout', () => { this.btnRightPressed = false; });
+        this.btnRight.on('pointerdown', () => {
+            this.btnRightPressed = true;
+            this.updateButtonStyle(this.rightBg, this.rightArrow, true);
+        });
+        this.btnRight.on('pointerup', () => {
+            this.btnRightPressed = false;
+            this.updateButtonStyle(this.rightBg, this.rightArrow, false);
+        });
+        this.btnRight.on('pointerout', () => {
+            this.btnRightPressed = false;
+            this.updateButtonStyle(this.rightBg, this.rightArrow, false);
+        });
 
         // Keyboard support (A/D, Arrow keys)
         if (this.scene.input.keyboard) {
-            this.scene.input.keyboard.on('keydown-A', () => { this.btnLeftPressed = true; });
-            this.scene.input.keyboard.on('keyup-A', () => { this.btnLeftPressed = false; });
-            this.scene.input.keyboard.on('keydown-D', () => { this.btnRightPressed = true; });
-            this.scene.input.keyboard.on('keyup-D', () => { this.btnRightPressed = false; });
-            this.scene.input.keyboard.on('keydown-LEFT', () => { this.btnLeftPressed = true; });
-            this.scene.input.keyboard.on('keyup-LEFT', () => { this.btnLeftPressed = false; });
-            this.scene.input.keyboard.on('keydown-RIGHT', () => { this.btnRightPressed = true; });
-            this.scene.input.keyboard.on('keyup-RIGHT', () => { this.btnRightPressed = false; });
+            this.scene.input.keyboard.on('keydown-A', () => {
+                this.btnLeftPressed = true;
+                this.updateButtonStyle(this.leftBg, this.leftArrow, true);
+            });
+            this.scene.input.keyboard.on('keyup-A', () => {
+                this.btnLeftPressed = false;
+                this.updateButtonStyle(this.leftBg, this.leftArrow, false);
+            });
+            this.scene.input.keyboard.on('keydown-D', () => {
+                this.btnRightPressed = true;
+                this.updateButtonStyle(this.rightBg, this.rightArrow, true);
+            });
+            this.scene.input.keyboard.on('keyup-D', () => {
+                this.btnRightPressed = false;
+                this.updateButtonStyle(this.rightBg, this.rightArrow, false);
+            });
+            this.scene.input.keyboard.on('keydown-LEFT', () => {
+                this.btnLeftPressed = true;
+                this.updateButtonStyle(this.leftBg, this.leftArrow, true);
+            });
+            this.scene.input.keyboard.on('keyup-LEFT', () => {
+                this.btnLeftPressed = false;
+                this.updateButtonStyle(this.leftBg, this.leftArrow, false);
+            });
+            this.scene.input.keyboard.on('keydown-RIGHT', () => {
+                this.btnRightPressed = true;
+                this.updateButtonStyle(this.rightBg, this.rightArrow, true);
+            });
+            this.scene.input.keyboard.on('keyup-RIGHT', () => {
+                this.btnRightPressed = false;
+                this.updateButtonStyle(this.rightBg, this.rightArrow, false);
+            });
+        }
+    }
+
+    private updateButtonStyle(bg: Phaser.GameObjects.Rectangle, arrow: Phaser.GameObjects.Text, pressed: boolean) {
+        if (pressed) {
+            bg.fillAlpha = 0.4;
+            bg.strokeAlpha = 0.7;
+            arrow.setScale(1.1);
+        } else {
+            bg.fillAlpha = 0.2;
+            bg.strokeAlpha = 0.4;
+            arrow.setScale(1);
         }
     }
 
